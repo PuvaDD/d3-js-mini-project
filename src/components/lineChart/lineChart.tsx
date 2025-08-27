426ff076f6d2f81516aae6a695177f161e572ccf
PuvaDD <pooyands16@gmail.com>
2025-08-28 03:20:14 +0330

    feat: Partial single/multi series detection

--- src/components/lineChart/lineChart.tsx
+++ src/components/lineChart/lineChart.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import "./lineChart.css";
 
 // Types for your data
@@ -33,6 +33,33 @@ const LineChart = ({
   const svgRef = useRef<SVGSVGElement | null>(null);
 
   const margin = { top: 50, right: 50, bottom: 50, left: 70 };
+
+  // DRAW CHART LOGIC
+  useEffect(() => {
+    if (!canvasRef.current || !svgRef.current) return;
+
+    const displayData = chart.data;
+    console.log("display Data = ", displayData);
+
+    const ctx = canvasRef.current.getContext("2d")!;
+    ctx.clearRect(0, 0, width, height);
+
+    // Calculate X and Y values for scales
+    const xValues = displayData.map((d) => d[0]);
+    const yValues: number[] = [];
+
+    displayData.forEach((d) => {
+      const y = d[1];
+      if (Array.isArray(y)) {
+        y.forEach((v) => {
+          if (v !== null && v !== undefined && !Number.isNaN(v))
+            yValues.push(v);
+        });
+      } else if (y !== null && y !== undefined && !Number.isNaN(y)) {
+        yValues.push(y);
+      }
+    });
+  }, [chart, width, height, maxPoints]);
 
   return (
     <div
